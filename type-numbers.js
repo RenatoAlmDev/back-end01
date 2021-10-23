22fb5472f25e395690f879a0a4dd11936b3c4b11
Renato Almeida <almeida.renato1988@gmail.com>
2021-10-23 16:44:45 -0300

    Testes da aula 1

---
+++ type-numbers.js
@@ -0,0 +1,22 @@
+// tipo number
+
+const meuNumero = 3;
+
+const num1 = 2;
+const num2 = 7;
+
+const operacaoMat = num1 + num2;
+
+const operacaoMat2 = num1 - num2;
+
+const operacaoMat3 = num1 * num2;
+
+const operacaoMat4 = num1 / num2;
+
+console.log(operacaoMat)
+
+console.log(operacaoMat2)
+
+console.log(operacaoMat3)
+
+console.log(operacaoMat4)
