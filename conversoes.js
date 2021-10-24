39df4e9a06dc273207de1d66d9eb60bcc134e3ff
Renato Almeida <almeida.renato1988@gmail.com>
2021-10-24 00:42:36 -0300

    Adicionando conteúdo Aula2

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dado
+// booleanos
+
+// conversao impicita
+const numero = 144;
+const numeroString = "144";
+
+//Number();
+//String();
+
+console.log(numero + Number(numeroString));
+
+//conversao explicita
---
+++ script.js
@@ -0,0 +1,40 @@
+//var
+
+var altura = 10;
+var comprimento = 15;
+
+area = altura * comprimento;
+console.log(area)
+var area;
+
+//let
+
+let forma = "retangulo"
+let altura = 10;
+let comprimento = 15;
+let area
+
+if (forma === "retangulo"){
+   area = altura * comprimento;
+} else {
+   area = (altura * comprimento) / 2;
+}
+
+area = altura * comprimento
+
+console.log(area)
+
+//const
+
+const forma = "triangulo";
+const altura = 10;
+const comprimento = 15;
+let area;
+
+if (forma === "quadrado") {
+   area = altura * comprimento; 
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area)
