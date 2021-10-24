742766463de49dc5dc42c677a03e8185113e7f6e
Renato Almeida <almeida.renato1988@gmail.com>
2021-10-24 10:27:19 -0300

    Adicionando exercícios Aula 4

---
+++ comparacoes.js
@@ -0,0 +1,18 @@
+// == (comparação imnplícita)
+
+const numero = 4;
+const texto = "4";
+
+console.log(numero == texto)
+
+//typeof
+console.log(typeof numero);
+console.log(typeof texto);
+
+// == só compara valor
+// === compara valor e tipo de dado
+
+//conversão explícita
+
+Number();
+String()
---
+++ template-strings.js
@@ -0,0 +1,10 @@
+const nome = "Renato";
+const idade = 2021-1988;
+const cidadeNascimento = "Divinópolis/MG";
+
+// const apresentacao = "Olá, meu nome é " + nome + ", tenho " + idade + " anos" + 
+// ", e nasci em " + cidadeNascimento;
+
+const apresentacao = `Olá, meu nome é ${nome}, tenho ${idade} anos, e nasci em ${cidadeNascimento}`;
+
+console.log(apresentacao)
